31d6688aed83310b43fe133bc07ae6711c323fdf
Fábio Sousa <73472338+FabSousa@users.noreply.github.com>
2023-09-20 08:12:28 -0300

    arquivo do professor

--- 2-Semestre/homeacademy/app/src/main/java/com/example/homeacademy/ExerciseActivity.kt
+++ 2-Semestre/homeacademy/app/src/main/java/com/example/homeacademy/ExerciseActivity.kt
@@ -145,7 +145,6 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             tts!!.shutdown()
         }
 
-
         if(player != null){
             player!!.stop()
         }
@@ -157,7 +156,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            val result = tts?.setLanguage(Locale.US)
+            val result = tts?.setLanguage(Locale.TRADITIONAL_CHINESE)
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.e("TTS", "The Language specified is not supported!")
             }
